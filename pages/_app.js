e251296f272f8b824db09ac249501a83cdad65d7
Alexsander <alexsander1996@yahoo.com.br>
2021-10-20 23:01:38 -0300

    Commit com projeto em andamento

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// ESTILOS GLOBAIS SO PODEM SER IMPORTADOS AQUI
+import '../styles/global.css';
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
